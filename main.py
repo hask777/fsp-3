7ad0e4ef945bca23b64e7181640d2b457a91a7f5
hask777 <haskellisp@gmail.com>
2023-06-08 11:31:55 +0300

    files

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from db import models
 from exceptions import StoryException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -46,3 +47,5 @@ app.add_middleware(
   allow_methods = ["*"],
   allow_headers = ['*']
 )
+
+app.mount('/files', StaticFiles(directory='files'), name='files')
--- router/files.py
+++ router/files.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, File
+from fastapi import APIRouter, File, UploadFile
+import shutil
+from fastapi.responses import FileResponse
 
 router = APIRouter(
     prefix='/file',
@@ -10,3 +12,21 @@ def get_file(files: bytes = File(...)):
     content = files.decode('utf-8')
     lines = content.split('\n')
     return {'lines': lines}
+
+@router.post('/uploadfile')
+def get_uploadfile(upload_file: UploadFile = File(...)):
+    path = f"files/{upload_file.filename}"
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+
+    return {
+        'filename': path,
+        'type': upload_file.content_type
+    }
+
+
+@router.get('/download/{name}', response_class=FileResponse)
+def get_file(name: str):
+    path = f'files/{name}'
+    return path
